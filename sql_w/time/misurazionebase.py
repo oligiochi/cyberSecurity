6a07ac47dad9592140767095c575e3fcdd4957d9
oligiochi <oligiovi2@gmail.com>
2025-10-10 18:30:23 +0200

    timesql

--- sql_w/time/misurazionebase.py
+++ sql_w/time/misurazionebase.py
@@ -4,9 +4,11 @@ import time
 
 inj = Inj('http://web-17.challs.olicyber.it')
 dictionary = '0123456789abcdef'
+r=inj.time('1')
+print(r)
 timestart=time.time()
-for i in range(1,10000):
+for i in range(1,100):
     inj.time(i)
 timeend=time.time()
 print(f"Time taken for 10k requests: {timeend-timestart} seconds")
-print("average time per request: {} seconds".format((timeend-timestart)/10000))
+print("average time per request: {} seconds".format((timeend-timestart)/100))
